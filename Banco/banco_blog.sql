8b86f95b2e33672c8b0c53035310706619000b92
achinov <gabrielarquilinoa.a@gmail.com>
2023-08-22 12:00:35 -0300

    mulilo

--- Banco/banco_blog.sql
+++ Banco/banco_blog.sql
@@ -23,7 +23,7 @@ CREATE TABLE post (
     data_postagem datetime NOT NULL,
     PRIMARY KEY(id),
     KEY fk_post_usuario_idx (usuario_id),
-    CONSTRAINT fk_post_usuario FOREIGN KEY (usuario_id) REFERENCES usuario (id),
+    CONSTRAINT fk_post_usuario FOREIGN KEY (usuario_id) REFERENCES usuario (id)
     );
 
 CREATE TABLE avaliacao (
---
+++ includes/topo.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+?>
+
+<div class="card">
+ <div class="card-header">
+<hl> Projeto Blog em PHP + MySQL IFSP - GABRIEL ARQUILINO</hl>
+
+ </div>
+<?php if(isset($_SESSION['login'])): ?>
+ <div class="card-body text-right">
+
+Olá <?php echo $_SESSION['login']['usuario']['nome']?>!
+<a href="core/usuario_ repositorio.php?acac=logout"
+class="btn btn-link btn-sm" role="button">Sair</a>
+ </div>
+<?php endif ?>
+</div>
